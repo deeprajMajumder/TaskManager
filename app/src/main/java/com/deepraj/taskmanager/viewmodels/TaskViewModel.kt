325e34d93d1479a93dd649af07874a21686d06e9
Deepraj <deeprajmajumdarknight@gmail.com>
2025-02-06 16:13:47 +0530

    Removed a text.

--- app/src/main/java/com/deepraj/taskmanager/viewmodels/TaskViewModel.kt
+++ app/src/main/java/com/deepraj/taskmanager/viewmodels/TaskViewModel.kt
@@ -72,7 +72,7 @@ class TaskViewModel @Inject constructor(
                     _tasks.value = taskDatabase.taskDao().getAllTasks()
                     _filteredTasks.value = _tasks.value // Initially no filtering, show all tasks
                 }
-                _uiState.value = TaskUiState.Loaded("Tasks Loaded")
+                _uiState.value = TaskUiState.Loaded("")
                 FirebaseAnalyticsUtil.logAnalyticsEvent(Constants.TASK_FETCHED_SUCCESS, true)
             } catch (e: Exception) {
                 _uiState.value = TaskUiState.Error("Network Error")
